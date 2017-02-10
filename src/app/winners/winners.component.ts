0c564a0c0057073abbf34bf4a9ba5a9490ea7149
SMI-WEBSERVER\burhan.mubarok <burhan.mubarok@sinergy.mediaintegrasi.com>
2017-02-10 16:34:33 +0700

    set data as input var

--- src/app/winners/winners.component.ts
+++ src/app/winners/winners.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-winners',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./winners.component.css']
 })
 export class WinnersComponent implements OnInit {
+  @Input() data: any;
 
   constructor() { }
 
